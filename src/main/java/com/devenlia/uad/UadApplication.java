d1ec38d5536bd95617a6475cd4eca6be49043e52
Jannis <j.machowetz@devenlia.com>
2023-09-17 14:40:39 +0200

    Initial commit

---
+++ src/main/java/com/devenlia/uad/UadApplication.java
@@ -0,0 +1,13 @@
+package com.devenlia.uad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devenlia/uad/UadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devenlia.uad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
